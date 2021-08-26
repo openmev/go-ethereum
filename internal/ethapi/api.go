aa7c8244a624df9489496f217d18a7cdc0ef9f29
sam bacha <sam@manifoldfinance.com>
2021-08-26 15:58:24 -0700

    jaeger (#3)
    Co-authored-by: Sina Mahmoodi <itz.s1na@gmail.com>

--- internal/ethapi/api.go
+++ internal/ethapi/api.go
@@ -46,6 +46,7 @@ import (
 	"github.com/ethereum/go-ethereum/params"
 	"github.com/ethereum/go-ethereum/rlp"
 	"github.com/ethereum/go-ethereum/rpc"
+	"github.com/opentracing/opentracing-go"
 	"github.com/tyler-smith/go-bip39"
 )
 
@@ -609,6 +610,8 @@ func NewPublicBlockChainAPI(b Backend) *PublicBlockChainAPI {
 
 // ChainId is the EIP-155 replay-protection chain id for the current ethereum chain config.
 func (api *PublicBlockChainAPI) ChainId() (*hexutil.Big, error) {
+	span := opentracing.StartSpan("ChainID")
+	defer span.Finish()
 	// if current block is at or past the EIP-155 replay-protection fork block, return chainID from config
 	if config := api.b.ChainConfig(); config.IsEIP155(api.b.CurrentBlock().Number()) {
 		return (*hexutil.Big)(config.ChainID), nil
@@ -618,6 +621,8 @@ func (api *PublicBlockChainAPI) ChainId() (*hexutil.Big, error) {
 
 // BlockNumber returns the block number of the chain head.
 func (s *PublicBlockChainAPI) BlockNumber() hexutil.Uint64 {
+	span := opentracing.StartSpan("BlockNumber")
+	defer span.Finish()
 	header, _ := s.b.HeaderByNumber(context.Background(), rpc.LatestBlockNumber) // latest header should always be available
 	return hexutil.Uint64(header.Number.Uint64())
 }
@@ -1605,11 +1610,14 @@ func (s *PublicTransactionPoolAPI) GetRawTransactionByHash(ctx context.Context, 
 
 // GetTransactionReceipt returns the transaction receipt for the given transaction hash.
 func (s *PublicTransactionPoolAPI) GetTransactionReceipt(ctx context.Context, hash common.Hash) (map[string]interface{}, error) {
-	tx, blockHash, blockNumber, index, err := s.b.GetTransaction(ctx, hash)
+	span := opentracing.StartSpan("GetTransaction")
+	defer span.Finish()
+	span.SetTag("hash", hash.Hex())
+	tx, blockHash, blockNumber, index, err := s.b.GetTransaction(opentracing.ContextWithSpan(ctx, span), hash)
 	if err != nil {
 		return nil, nil
 	}
-	receipts, err := s.b.GetReceipts(ctx, blockHash)
+	receipts, err := s.b.GetReceipts(opentracing.ContextWithSpan(ctx, span), blockHash)
 	if err != nil {
 		return nil, err
 	}
